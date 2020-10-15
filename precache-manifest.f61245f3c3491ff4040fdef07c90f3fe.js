f883b255ad7212991cee0b175a3d45162118dca2
elosorulo <manustrajman@gmail.com>
2020-10-15 00:08:18 -0300

    Updates

---
+++ precache-manifest.f61245f3c3491ff4040fdef07c90f3fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5235ecaabcf7f5963f12113d2e8483d",
+    "url": "/tutorial-pav/index.html"
+  },
+  {
+    "revision": "66f667d2400c5cd15d24",
+    "url": "/tutorial-pav/static/css/2.c84d9256.chunk.css"
+  },
+  {
+    "revision": "18e2de8fc0a265ffc81e",
+    "url": "/tutorial-pav/static/css/main.8c547140.chunk.css"
+  },
+  {
+    "revision": "66f667d2400c5cd15d24",
+    "url": "/tutorial-pav/static/js/2.24df03cb.chunk.js"
+  },
+  {
+    "revision": "5544b97da431ba1cb20d5aa93555fb4f",
+    "url": "/tutorial-pav/static/js/2.24df03cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e2de8fc0a265ffc81e",
+    "url": "/tutorial-pav/static/js/main.eaca128f.chunk.js"
+  },
+  {
+    "revision": "0b71736aff9fcc3faccf",
+    "url": "/tutorial-pav/static/js/runtime-main.72d2e7d7.js"
+  }
+]);
